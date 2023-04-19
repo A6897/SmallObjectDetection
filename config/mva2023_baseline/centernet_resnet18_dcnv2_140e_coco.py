1c4fcb9dd3378ba15a98764be5b2f01563c548f7
Revathi Vipinachandran <revathivch@gmail.com>
2023-04-19 10:47:00 -0400

    Corrected folder structure

--- config/mva2023_baseline/centernet_resnet18_dcnv2_140e_coco.py
+++ config/mva2023_baseline/centernet_resnet18_dcnv2_140e_coco.py
@@ -1,4 +1,5 @@
 _base_ = [
+    # '../_base_/datasets/coco_detection.py',
     './drone_dataset.py',
     '../_base_/schedules/schedule_1x.py', '../_base_/default_runtime.py'
 ]
@@ -7,92 +8,47 @@ model = dict(
     type='CenterNet',
     backbone=dict(
         type='ResNet',
-        depth=50,  # Change to ResNet50
+        depth=18,
         norm_eval=False,
         norm_cfg=dict(type='BN'),
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18')),
     neck=dict(
-        type='CTResNetNeck',
-        in_channel=2048,  # Update in_channel for ResNet50
-        num_deconv_filters=(256, 128, 64),
-        num_deconv_kernels=(4, 4, 4),
-        use_dcn=True),
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=5),
+        # in_channel=512,
+        # num_deconv_filters=(256, 128, 64),
+        # num_deconv_kernels=(4, 4, 4),
+        # use_dcn=True),
     bbox_head=dict(
         type='CenterNetHead',
-        num_classes=1,
+        num_classes=1,  # 80
         in_channel=64,
         feat_channel=64,
         loss_center_heatmap=dict(type='GaussianFocalLoss', loss_weight=1.0),
-        loss_wh=dict(type='L1Loss', loss_weight=0.1),  # Use GIoU loss
+        loss_wh=dict(type='L1Loss', loss_weight=0.1),
         loss_offset=dict(type='L1Loss', loss_weight=1.0)),
     train_cfg=None,
     test_cfg=dict(topk=100, local_maximum_kernel=3, max_per_img=100))
 
-img_scale = (512, 512)  # Increase input image size
-img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-
-# Data augmentation
-data = dict(
-    samples_per_gpu=4,
-    workers_per_gpu=2,
-    train=dict(
-        pipeline=[
-            dict(type='LoadImageFromFile'),
-            dict(type='LoadAnnotations', with_bbox=True),
-            dict(type='Resize', img_scale=img_scale, keep_ratio=True),
-            dict(type='RandomFlip', flip_ratio=0.5),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='DefaultFormatBundle'),
-            dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
-        ]),
-    val=dict(pipeline=[
-        dict(type='LoadImageFromFile'),
-        dict(type='MultiScaleFlipAug', img_scale=img_scale, flip=False, transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-#             dict(type='ImageToTensor', keys=['img']),
-            dict(type='DefaultFormatBundle'),
-            dict(
-                type='Collect',
-                meta_keys=('filename', 'ori_shape', 'img_shape', 'pad_shape',
-                           'scale_factor', 'flip', 'flip_direction',
-                           'img_norm_cfg', 'border'),
-                keys=['img']),
-        ])
-    ]),
-    test=dict(pipeline=[
-        dict(type='LoadImageFromFile'),
-        dict(type='MultiScaleFlipAug', img_scale=img_scale, flip=False, transforms=[
-            dict(type='Resize', keep_ratio=True),
-            dict(type='RandomFlip'),
-            dict(type='Normalize', **img_norm_cfg),
-            dict(type='Pad', size_divisor=32),
-            dict(type='ImageToTensor', keys=['img']),
-            dict(
-                type='Collect',
-                meta_keys=('filename', 'ori_shape', 'img_shape', 'pad_shape',
-                           'scale_factor', 'flip', 'flip_direction',
-                           'img_norm_cfg', 'border'),
-                keys=['img']),
-        ])
-    ])
-)
-
+# optimizer
+# Based on the default settings of modern detectors, the SGD effect is better
+# than the Adam in the source code, so we use SGD default settings and
+# if you use adam+lr5e-4, the map is 29.1.
 optimizer_config = dict(
     _delete_=True, grad_clip=dict(max_norm=35, norm_type=2))
 
-# Fine-tune learning rate and other hyperparameters
+# learning policy
+# Based on the default settings of modern detectors, we added warmup settings.
 lr_config = dict(
     policy='step',
     warmup='linear',
     warmup_iters=1000,
     warmup_ratio=1.0 / 1000,
     step=[90, 120])
-runner = dict(max_epochs=50)
+runner = dict(max_epochs=10)
 
-evaluation = dict(interval=5, metric='bbox')
-checkpoint_config = dict(interval=5)
+# Avoid evaluation and saving weights too frequently
+evaluation = dict(interval=1, metric='bbox')
+checkpoint_config = dict(interval=1)
